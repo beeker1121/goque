65d78fc5b3e3abd8baf21018492744ab54ce132c
Conner Hewitt <connerh@navilytics.com>
2016-06-14 21:54:13 -0500

    Updated error messages again

--- errors.go
+++ errors.go
@@ -10,14 +10,14 @@ var (
 	ErrIncompatibleType = errors.New("goque: Opener type is incompatible with stored Goque type")
 
 	// ErrEmpty is returned when the stack or queue is empty.
-	ErrEmpty = errors.New("goque: The stack or queue is empty")
+	ErrEmpty = errors.New("goque: Stack or queue is empty")
 
 	// ErrOutOfBounds is returned when the ID used to lookup an item
-	// is outside the range of the stack or queue.
-	ErrOutOfBounds = errors.New("goque: ID used is outside the range of the stack or queue")
+	// is outside of the range of the stack or queue.
+	ErrOutOfBounds = errors.New("goque: ID used is outside range of stack or queue")
 
 	// ErrDBClosed is returned when the Close function has already
 	// been called, causing the stack or queue to close, as well as
 	// its underlying database.
-	ErrDBClosed = errors.New("goque: The database is closed")
+	ErrDBClosed = errors.New("goque: Database is closed")
 )
