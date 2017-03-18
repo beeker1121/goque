8694707573c7aa6a74281ed37315940a9389a764
Conner Hewitt <connerh87@gmail.com>
2017-03-18 10:02:55 -0500

    Added example test for prefix queue and fixed comment in priority queue example test

--- example_prefix_queue_test.go
+++ example_prefix_queue_test.go
@@ -6,31 +6,30 @@ import (
 	"github.com/beeker1121/goque"
 )
 
-// ExamplePriorityQueue demonstrates the implementation of a Goque queue.
-func Example_priorityQueue() {
-	// Open/create a priority queue.
-	pq, err := goque.OpenPriorityQueue("data_dir", goque.ASC)
+// ExamplePrefixQueue demonstrates the implementation of a Goque queue.
+func Example_prefixQueue() {
+	// Open/create a prefix queue.
+	pq, err := goque.OpenPrefixQueue("data_dir")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer pq.Close()
 
-	// Enqueue the item.
-	item, err := pq.Enqueue(0, []byte("item value"))
+	// Enqueue an item.
+	item, err := pq.Enqueue([]byte("prefix"), []byte("item value"))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	fmt.Println(item.ID)         // 1
-	fmt.Println(item.Priority)   // 0
-	fmt.Println(item.Key)        // [0 58 0 0 0 0 0 0 0 1]
+	fmt.Println(item.Key)        // [112 114 101 102 105 120 0 0 0 0 0 0 0 0 1]
 	fmt.Println(item.Value)      // [105 116 101 109 32 118 97 108 117 101]
 	fmt.Println(item.ToString()) // item value
 
 	// Change the item value in the queue.
-	item, err = pq.Update(item.Priority, item.ID, []byte("new item value"))
+	item, err = pq.Update([]byte("prefix"), item.ID, []byte("new item value"))
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -39,7 +38,7 @@ func Example_priorityQueue() {
 	fmt.Println(item.ToString()) // new item value
 
 	// Dequeue the next item.
-	deqItem, err := pq.Dequeue()
+	deqItem, err := pq.Dequeue([]byte("prefix"))
 	if err != nil {
 		fmt.Println(err)
 		return
